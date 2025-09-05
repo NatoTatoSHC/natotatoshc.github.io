93b9c61f047c7fb41a2683f069f065c767db6e88
NatoTatoSHC <lesternate6@gmail.com>
2025-09-05 18:39:03 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+const sections = {
+  gametab: "games",
+  classictab: "classics",
+  tooltab: "tools",
+  abouttab: "about",
+  contacttab: "contacts"
+};
+
+Object.keys(sections).forEach(tabId => {
+  document.getElementById(tabId).onclick = () => {
+    Object.values(sections).forEach(sectionId => {
+      document.getElementById(sectionId).hidden = true;
+    });
+    document.getElementById(sections[tabId]).hidden = false;
+  };
+});
+
+document.getElementById("QC").onclick = () => {
+  location.assign("https://natotatoshc.github.io/QuantumClicker");
+};
+document.getElementById("J").onclick = () => {
+  location.assign("https://natotatoshc.github.io/Jokes");
+};
+document.getElementById("rala").onclick = () => {
+  // location.assign("https://natotatoshc.github.io/Ralandia");
+};
+document.getElementById("NWA").onclick = () => {
+  location.assign("https://natotatoshc.github.io/NateWorks-Attack");
+};
